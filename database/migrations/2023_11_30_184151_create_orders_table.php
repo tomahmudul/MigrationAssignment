eb041298a4bd0ba26310899c1492c4bc3f79fb0a
Mahmudul Hassan <hassan.01303@gmail.com>
2023-12-01 23:27:08 +0600

    assignment 10

--- database/migrations/2023_11_30_184151_create_orders_table.php
+++ database/migrations/2023_11_30_184151_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id('id');
-            $table->foreignId('product_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();   
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->integer('quantity');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
